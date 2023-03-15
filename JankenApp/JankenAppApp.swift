61f7f9b4325630e1c1e1a100fd0dc6650ac33da7
rsugaya <rsugaya@llc-noa.net>
2023-03-15 18:22:59 +0900

    Initial Commit

---
+++ JankenApp/JankenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JankenAppApp.swift
+//  JankenApp
+//
+//  Created by 菅谷亮太 on 2023/03/15.
+//
+
+import SwiftUI
+
+@main
+struct JankenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
